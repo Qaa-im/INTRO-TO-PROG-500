95fd3a65e967628a3523a7fb8364a17368be2eea
Qaa-im <165993489+Qaa-im@users.noreply.github.com>
2024-06-11 12:25:11 +0200

    Add files via upload

---
+++ Revition_VB/Form1.Designer.vb
@@ -0,0 +1,563 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        Button1 = New Button()
+        Label1 = New Label()
+        ListBox1 = New ListBox()
+        GroupBox2 = New GroupBox()
+        Button3 = New Button()
+        Button2 = New Button()
+        GroupBox3 = New GroupBox()
+        TextBox1 = New TextBox()
+        Button4 = New Button()
+        GroupBox4 = New GroupBox()
+        TextBox2 = New TextBox()
+        Button6 = New Button()
+        Button5 = New Button()
+        ListBox2 = New ListBox()
+        GroupBox5 = New GroupBox()
+        Button7 = New Button()
+        Button8 = New Button()
+        ListBox3 = New ListBox()
+        GroupBox6 = New GroupBox()
+        NumericUpDown2 = New NumericUpDown()
+        NumericUpDown1 = New NumericUpDown()
+        Button10 = New Button()
+        ListBox4 = New ListBox()
+        GroupBox7 = New GroupBox()
+        TextBox4 = New TextBox()
+        TextBox3 = New TextBox()
+        Button9 = New Button()
+        GroupBox8 = New GroupBox()
+        Label2 = New Label()
+        TextBox6 = New TextBox()
+        Button11 = New Button()
+        GroupBox9 = New GroupBox()
+        Label3 = New Label()
+        TextBox5 = New TextBox()
+        Button12 = New Button()
+        GroupBox10 = New GroupBox()
+        Label4 = New Label()
+        Button13 = New Button()
+        GroupBox11 = New GroupBox()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        Button14 = New Button()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        GroupBox3.SuspendLayout()
+        GroupBox4.SuspendLayout()
+        GroupBox5.SuspendLayout()
+        GroupBox6.SuspendLayout()
+        CType(NumericUpDown2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(NumericUpDown1, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBox7.SuspendLayout()
+        GroupBox8.SuspendLayout()
+        GroupBox9.SuspendLayout()
+        GroupBox10.SuspendLayout()
+        GroupBox11.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = SystemColors.ActiveBorder
+        GroupBox1.Controls.Add(Button1)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Location = New Point(22, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(185, 104)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "SUM"' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(39, 33)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 3
+        Button1.Text = "SUM"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(60, 68)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Label1"' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(25, 22)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(120, 184)
+        ListBox1.TabIndex = 2
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.BackColor = Color.FromArgb(CByte(128), CByte(128), CByte(255))
+        GroupBox2.Controls.Add(Button3)
+        GroupBox2.Controls.Add(Button2)
+        GroupBox2.Controls.Add(ListBox1)
+        GroupBox2.Location = New Point(3, 249)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(185, 269)
+        GroupBox2.TabIndex = 3
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "GroupBox2"' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(39, 241)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 4
+        Button3.Text = "Swap"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(39, 212)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 3
+        Button2.Text = "Display"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox3
+        ' 
+        GroupBox3.BackColor = SystemColors.HotTrack
+        GroupBox3.Controls.Add(TextBox1)
+        GroupBox3.Controls.Add(Button4)
+        GroupBox3.Location = New Point(22, 131)
+        GroupBox3.Name = "GroupBox3"
+        GroupBox3.Size = New Size(185, 104)
+        GroupBox3.TabIndex = 4
+        GroupBox3.TabStop = False
+        GroupBox3.Text = "SUM"' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(41, 60)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(50, 22)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(75, 23)
+        Button4.TabIndex = 3
+        Button4.Text = "SUM"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox4
+        ' 
+        GroupBox4.BackColor = Color.FromArgb(CByte(0), CByte(64), CByte(64))
+        GroupBox4.Controls.Add(TextBox2)
+        GroupBox4.Controls.Add(Button6)
+        GroupBox4.Controls.Add(Button5)
+        GroupBox4.Controls.Add(ListBox2)
+        GroupBox4.Location = New Point(198, 249)
+        GroupBox4.Name = "GroupBox4"
+        GroupBox4.Size = New Size(185, 269)
+        GroupBox4.TabIndex = 5
+        GroupBox4.TabStop = False
+        GroupBox4.Text = "GroupBox4"' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(39, 22)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 6
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(55, 51)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(75, 23)
+        Button6.TabIndex = 5
+        Button6.Text = "Search"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(55, 240)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(75, 23)
+        Button5.TabIndex = 4
+        Button5.Text = "Display"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox2
+        ' 
+        ListBox2.FormattingEnabled = True
+        ListBox2.ItemHeight = 15
+        ListBox2.Location = New Point(39, 96)
+        ListBox2.Name = "ListBox2"
+        ListBox2.Size = New Size(120, 139)
+        ListBox2.TabIndex = 2
+        ' 
+        ' GroupBox5
+        ' 
+        GroupBox5.BackColor = SystemColors.ActiveCaption
+        GroupBox5.Controls.Add(Button7)
+        GroupBox5.Controls.Add(Button8)
+        GroupBox5.Controls.Add(ListBox3)
+        GroupBox5.Location = New Point(399, 249)
+        GroupBox5.Name = "GroupBox5"
+        GroupBox5.Size = New Size(185, 269)
+        GroupBox5.TabIndex = 6
+        GroupBox5.TabStop = False
+        GroupBox5.Text = "GroupBox5"' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(39, 240)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(75, 23)
+        Button7.TabIndex = 4
+        Button7.Text = "SORT"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(39, 212)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(75, 23)
+        Button8.TabIndex = 3
+        Button8.Text = "Display"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox3
+        ' 
+        ListBox3.FormattingEnabled = True
+        ListBox3.ItemHeight = 15
+        ListBox3.Location = New Point(25, 22)
+        ListBox3.Name = "ListBox3"
+        ListBox3.Size = New Size(120, 184)
+        ListBox3.TabIndex = 2
+        ' 
+        ' GroupBox6
+        ' 
+        GroupBox6.BackColor = Color.Gray
+        GroupBox6.Controls.Add(NumericUpDown2)
+        GroupBox6.Controls.Add(NumericUpDown1)
+        GroupBox6.Controls.Add(Button10)
+        GroupBox6.Controls.Add(ListBox4)
+        GroupBox6.Location = New Point(227, 12)
+        GroupBox6.Name = "GroupBox6"
+        GroupBox6.Size = New Size(430, 223)
+        GroupBox6.TabIndex = 7
+        GroupBox6.TabStop = False
+        GroupBox6.Text = "GroupBox6"' 
+        ' NumericUpDown2
+        ' 
+        NumericUpDown2.Location = New Point(6, 68)
+        NumericUpDown2.Name = "NumericUpDown2"
+        NumericUpDown2.Size = New Size(120, 23)
+        NumericUpDown2.TabIndex = 5
+        ' 
+        ' NumericUpDown1
+        ' 
+        NumericUpDown1.Location = New Point(6, 39)
+        NumericUpDown1.Name = "NumericUpDown1"
+        NumericUpDown1.Size = New Size(120, 23)
+        NumericUpDown1.TabIndex = 4
+        ' 
+        ' Button10
+        ' 
+        Button10.Location = New Point(26, 97)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(75, 23)
+        Button10.TabIndex = 3
+        Button10.Text = "Display"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' ListBox4
+        ' 
+        ListBox4.FormattingEnabled = True
+        ListBox4.ItemHeight = 15
+        ListBox4.Location = New Point(139, 22)
+        ListBox4.Name = "ListBox4"
+        ListBox4.Size = New Size(270, 184)
+        ListBox4.TabIndex = 2
+        ' 
+        ' GroupBox7
+        ' 
+        GroupBox7.BackColor = Color.Gray
+        GroupBox7.Controls.Add(TextBox4)
+        GroupBox7.Controls.Add(TextBox3)
+        GroupBox7.Controls.Add(Button9)
+        GroupBox7.Location = New Point(590, 394)
+        GroupBox7.Name = "GroupBox7"
+        GroupBox7.Size = New Size(245, 124)
+        GroupBox7.TabIndex = 8
+        GroupBox7.TabStop = False
+        GroupBox7.Text = "GroupBox7"' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(58, 95)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 8
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(58, 22)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 7
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(67, 56)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(75, 23)
+        Button9.TabIndex = 3
+        Button9.Text = "Display"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox8
+        ' 
+        GroupBox8.BackColor = Color.Gray
+        GroupBox8.Controls.Add(Label2)
+        GroupBox8.Controls.Add(TextBox6)
+        GroupBox8.Controls.Add(Button11)
+        GroupBox8.Location = New Point(590, 249)
+        GroupBox8.Name = "GroupBox8"
+        GroupBox8.Size = New Size(245, 127)
+        GroupBox8.TabIndex = 9
+        GroupBox8.TabStop = False
+        GroupBox8.Text = "GroupBox8"' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(83, 96)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(41, 15)
+        Label2.TabIndex = 8
+        Label2.Text = "Label2"' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(58, 22)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(100, 23)
+        TextBox6.TabIndex = 7
+        ' 
+        ' Button11
+        ' 
+        Button11.Location = New Point(67, 56)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(75, 23)
+        Button11.TabIndex = 3
+        Button11.Text = "Check"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox9
+        ' 
+        GroupBox9.BackColor = Color.FromArgb(CByte(192), CByte(192), CByte(255))
+        GroupBox9.Controls.Add(Label3)
+        GroupBox9.Controls.Add(TextBox5)
+        GroupBox9.Controls.Add(Button12)
+        GroupBox9.Location = New Point(663, 12)
+        GroupBox9.Name = "GroupBox9"
+        GroupBox9.Size = New Size(202, 104)
+        GroupBox9.TabIndex = 10
+        GroupBox9.TabStop = False
+        GroupBox9.Text = "GroupBox9"' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(83, 82)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(41, 15)
+        Label3.TabIndex = 8
+        Label3.Text = "Label3"' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(58, 22)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(100, 23)
+        TextBox5.TabIndex = 7
+        ' 
+        ' Button12
+        ' 
+        Button12.Location = New Point(71, 51)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(75, 23)
+        Button12.TabIndex = 3
+        Button12.Text = "Check"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox10
+        ' 
+        GroupBox10.BackColor = SystemColors.HotTrack
+        GroupBox10.Controls.Add(Label4)
+        GroupBox10.Controls.Add(Button13)
+        GroupBox10.Location = New Point(680, 131)
+        GroupBox10.Name = "GroupBox10"
+        GroupBox10.Size = New Size(185, 104)
+        GroupBox10.TabIndex = 11
+        GroupBox10.TabStop = False
+        GroupBox10.Text = "Average"' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(62, 60)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(41, 15)
+        Label4.TabIndex = 4
+        Label4.Text = "Label4"' 
+        ' Button13
+        ' 
+        Button13.Location = New Point(50, 22)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(75, 23)
+        Button13.TabIndex = 3
+        Button13.Text = "Average"
+        Button13.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox11
+        ' 
+        GroupBox11.BackColor = Color.IndianRed
+        GroupBox11.Controls.Add(TextBox7)
+        GroupBox11.Controls.Add(TextBox8)
+        GroupBox11.Controls.Add(Button14)
+        GroupBox11.Location = New Point(862, 315)
+        GroupBox11.Name = "GroupBox11"
+        GroupBox11.Size = New Size(245, 124)
+        GroupBox11.TabIndex = 12
+        GroupBox11.TabStop = False
+        GroupBox11.Text = "GroupBox11"' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(58, 95)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(100, 23)
+        TextBox7.TabIndex = 8
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(58, 22)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(100, 23)
+        TextBox8.TabIndex = 7
+        ' 
+        ' Button14
+        ' 
+        Button14.Location = New Point(67, 56)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(75, 23)
+        Button14.TabIndex = 3
+        Button14.Text = "SWAP"
+        Button14.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1149, 530)
+        Controls.Add(GroupBox11)
+        Controls.Add(GroupBox10)
+        Controls.Add(GroupBox9)
+        Controls.Add(GroupBox8)
+        Controls.Add(GroupBox7)
+        Controls.Add(GroupBox6)
+        Controls.Add(GroupBox5)
+        Controls.Add(GroupBox4)
+        Controls.Add(GroupBox3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox3.ResumeLayout(False)
+        GroupBox3.PerformLayout()
+        GroupBox4.ResumeLayout(False)
+        GroupBox4.PerformLayout()
+        GroupBox5.ResumeLayout(False)
+        GroupBox6.ResumeLayout(False)
+        CType(NumericUpDown2, ComponentModel.ISupportInitialize).EndInit()
+        CType(NumericUpDown1, ComponentModel.ISupportInitialize).EndInit()
+        GroupBox7.ResumeLayout(False)
+        GroupBox7.PerformLayout()
+        GroupBox8.ResumeLayout(False)
+        GroupBox8.PerformLayout()
+        GroupBox9.ResumeLayout(False)
+        GroupBox9.PerformLayout()
+        GroupBox10.ResumeLayout(False)
+        GroupBox10.PerformLayout()
+        GroupBox11.ResumeLayout(False)
+        GroupBox11.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents GroupBox6 As GroupBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Button10 As Button
+    Friend WithEvents ListBox4 As ListBox
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents GroupBox7 As GroupBox
+    Friend WithEvents Button9 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents GroupBox8 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button11 As Button
+    Friend WithEvents GroupBox9 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button12 As Button
+    Friend WithEvents GroupBox10 As GroupBox
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox11 As GroupBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Button14 As Button
+End Class
---
+++ Revition_VB/Form1.vb
@@ -0,0 +1,201 @@
+﻿Imports System.Threading.Tasks.Sources
+
+Module ArrayNames
+    Public arrnames() As String = {"Alice", "Bob", "Charlie", "Diana", "Eve", "Frank", "Grace", "Hannah", "Ivy", "Jack"}
+End Module
+
+
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim arrNum() As Integer = {3, 7, 2, 6, 1, 8, 3, 8, 3}
+        Dim iTotal As Integer
+
+        iTotal = 0
+
+        For i = 0 To (arrNum.Length - 1)
+            iTotal = iTotal + arrNum(i)
+        Next
+
+        Label1.Text = "Total : " + iTotal.ToString
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox1.Items.Clear()
+        For i = 0 To (arrnames.Length - 1)
+            ListBox1.Items.Add(arrnames(i))
+
+        Next
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Clear()
+        For i = (arrnames.Length - 1) To 0 Step -1
+            ListBox1.Items.Add(arrnames(i))
+        Next
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim arrNum(,) As Integer = {{1, 4, 7, 9}, {2, 6, 8, 2}, {7, 2, 8, 6}}
+        Dim iTotal As Integer
+        iTotal = 0
+
+        For i = 0 To 2
+            For j = 0 To 3
+                iTotal = iTotal + arrNum(i, j)
+            Next
+        Next
+
+        TextBox1.Text = "Toatal : " + iTotal.ToString
+
+    End Sub
+
+
+    Private Sub Button5_Click_1(sender As Object, e As EventArgs) Handles Button5.Click
+
+        ListBox1.Items.Clear()
+        For i = 0 To (arrnames.Length - 1)
+            ListBox2.Items.Add(arrnames(i))
+
+        Next
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim sname As String
+        Dim Flag As Boolean
+        sname = TextBox2.Text
+        Flag = False
+
+        For i = 0 To (arrnames.Length - 1)
+            If sname.ToUpper = arrnames(i).ToUpper Then
+                MessageBox.Show("Name Found")
+                Flag = True
+                Exit For
+            End If
+        Next
+
+        If Flag = False Then
+            MessageBox.Show("Name Not Found")
+        End If
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        ListBox3.Items.Clear()
+        For i = 0 To (arrnames.Length - 1)
+            ListBox3.Items.Add(arrnames(i))
+
+        Next
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+        ListBox3.Items.Clear()
+        Array.Sort(arrnames)
+        For i = (arrnames.Length - 1) To 0 Step -1
+            ListBox3.Items.Add(arrnames(i))
+        Next
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Dim inum, leng As Integer
+
+        inum = NumericUpDown1.Value
+        leng = NumericUpDown2.Value
+
+        ListBox4.Items.Clear()
+        ListBox4.Items.Add("MY MULTIPLICATION TABLE")
+        ListBox4.Items.Add("")
+
+        For i = 1 To leng
+            ListBox4.Items.Add(inum.ToString() & " x " & i.ToString() & " = " & (inum * i).ToString())
+        Next
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim inum As Integer
+        Dim Fac As Integer
+        Dim i As Integer
+
+        inum = Val(TextBox3.Text)
+        Fac = 1
+        i = 1
+
+        While i < inum
+            i = i + 1
+            Fac = Fac * i
+        End While
+
+
+        TextBox4.Text = Fac.ToString
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Dim age As Integer
+
+        age = Val(TextBox6.Text)
+        If age > 18 Then
+            Label2.Text = "Egible age"
+        Else
+            Label2.Text = "Not Egible"
+        End If
+
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Dim inum As Integer
+        inum = Val(TextBox5.Text)
+
+        Select Case inum
+            Case Is > 0
+                Label3.Text = "Positive number"
+            Case Is < 0
+                Label3.Text = "Neggative number"
+            Case = 0
+                Label3.Text = "Number is 0"
+        End Select
+
+
+
+    End Sub
+
+    Private Sub CalculateAverage(arr() As Integer, ByRef sum As Integer)
+        For i = 0 To (arr.Length - 1)
+            sum = sum + arr(i)
+        Next
+    End Sub
+
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Dim arrNum() As Integer = {3, 7, 2, 6, 1, 8, 3, 8, 3}
+        Dim sum As Integer = 0
+        Dim average As Double
+
+        CalculateAverage(arrNum, sum)
+
+        average = sum / arrNum.Length
+
+        MessageBox.Show("The average is: " & average)
+    End Sub
+
+    Function Swap(sword As String) As String
+        Dim snew As String = ""
+
+        For i As Integer = sword.Length - 1 To 0 Step -1
+            snew &= sword(i)
+        Next
+
+        Return snew
+    End Function
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Dim sinput As String
+        sinput = TextBox8.Text
+        TextBox7.Text = Swap(sinput)
+    End Sub
+
+
+End Class
